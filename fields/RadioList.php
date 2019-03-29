e2ce7176c2168263e48c0cb6f7c089d3559d8f4b
rmakhambetov <rmakhambetov@yandex.ru>
2019-03-29 16:02:07 +0600

    Show input label for radioList

--- fields/RadioList.php
+++ fields/RadioList.php
@@ -1,12 +1,20 @@
 <?php
+
 namespace fgh151\fields\fields;
+
+use yii\helpers\Html;
 
 class RadioList extends BaseField
 {
-    public $variants;
+    public $variants = [];
     public $htmlType = 'radio';
+
     public function render()
     {
+        if (!empty($this->label)) {
+            echo Html::label($this->label, $this->attribute);
+        }
+        
         foreach ($this->variants as $variant) {
             echo BaseField::create('BinaryRadio', [
                 'label' => $variant['label'],
